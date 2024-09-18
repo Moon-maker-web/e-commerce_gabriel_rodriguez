d16c07d675fd09b68689d313cbf4e9255388ed0c
¨moon¨ <moon@moon.com>
2024-09-17 20:26:00 -0600

    modifique index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-function saludar() {
-    alert("¡Hola, mundo!");
-}
+const card1 = document.querySelector('#card1');
+const card2 = document.querySelector('#card2');
+const card3 = document.querySelector('#card3');
+
+
+card1.querySelector('h5').textContent = 'PRODUCTO 1';
+card1.querySelector('p').textContent = 'ESTE PRODUCTO ES FABULOSO';
+card1.style.backgroundColor = '#0969f5';  
+
+card2.querySelector('h2').textContent = 'PRODUCTO 2';
+card2.querySelector('p').textContent = 'ESTE PRODUCTO ES LO MEJOR';
+card2.style.backgroundColor = '#10f509';  
+
+card3.querySelector('h2').textContent = 'PRODUCTO 3';
+card3.querySelector('p').textContent = 'ESTE PRODUCTO ES EL MEJOR VENDIDO';
+card3.style.backgroundColor = '#f50909';  
